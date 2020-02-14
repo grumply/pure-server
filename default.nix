f9e86c0664750f5e8d0404d41cdae2d0db0ca7e2
Sean Hickman <sean@grumply.com>
2020-02-13 23:35:46 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
 }:
 mkDerivation {
   pname = "pure-websocket";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base containers pure-core pure-default pure-websocket ];
   configureFlags = [ (secure ? "-fsecure") ];
